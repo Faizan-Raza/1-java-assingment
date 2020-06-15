271ed4fd173f87f377ba21a2829ef413ce91fa13
unknown <faizanraza.07022004@gmail.com>
2020-06-16 01:24:10 +0500

    hi world

---
+++ app.js
@@ -0,0 +1,130 @@
+// == Chapter #1 == Task#1
+alert("WEllCOME TO MY WEBSITE")
+// == Chapter #1 == Task#2
+alert("Error! Please enter a valid password")
+// == Chapter #1 == Task#3
+alert("Welcome to JS Land...\nHappy Coding!")
+// == Chapter #1 == Task#5
+var a=("Hi i am her in console")
+console.log(a)
+alert(a)
+// == Chapter #2 == Task#1
+var username = ""
+// == Chapter #2 == Task#2
+ var myname = "Muhammad Faizan Raza Memon"
+ // == Chapter #2 == Task#3
+ var message = "HI WORLD";
+ alert(message);
+ // == Chapter #2 == Task#4
+ var studentname = "Arif Khan";
+ alert(studentname);
+ var studentage = "17 years old";
+ alert(studentage);
+ var studentcertification = "full stake web developer";
+ alert(studentcertification)
+ // == Chapter #2 == Task#5
+var menu = "pizza\npizz\npiz\npi\np";
+alert(menu);
+ // == Chapter #2 == Task#6
+var email = "faizanraza.07022004@gmail.com";
+var myemail = "My EMAIL Address is ";
+var emailalert = myemail+email;
+alert(emailalert);
+ // == Chapter #2 == Task#7
+ var book = "a smarter way to learn java script";
+ var learn = "i am trying to leran from the book ";
+ var learnalert = learn+book;
+alert(learnalert);
+ // == Chapter #2 == Task#8
+var design = " ---- --`~`~`~`-- ---- ";
+alert(design);
+
+ // == Chapter #3 == Task#1
+ var age = "17";
+ alert("i am "+age+" years old");
+ // == Chapter #3 == Task#3
+var birthyear = "2004";
+document.write("my birth year is "+birthyear);
+// == Chapter #3 == Task#4
+var visitorsname = "faizan";
+var producttittle = "6t-shirts";
+var storename = "memonclothing";
+document.write(visitorsname+"ordered"+producttittle+"on"+storename)
+
+ // == Chapter #4 == Task#1
+var myName = ""
+var myAge = ""
+var mySchool = ""
+ // == Chapter 4 == Task#2
+var numberOne = ""
+var numberTwo = ""
+var numberThree = ""
+var numberFour = ""
+var numberFive = ""
+// var 5Number = ""
+// var 4*Number = ""
+// var 2#Number = ""
+// var @NUMBER3 = ""
+// var %number1 = ""
+ // == Chapter #4 == Task#3
+
+ // == Chapter #5 == Task#1
+var a = 12;
+var b = 9;
+var c = a+b;
+document.write("sum of "+ a +" and "+ b +" is "+ c);
+ // == Chapter #5 == Task#2
+ var a = 12;
+var b = 9;
+var c = a-b;
+document.write("subtraction of "+ a +" and "+ b +" is "+ c);
+var a = 12;
+var b = 9;
+var c = a*b;
+document.write("multiplication of "+ a +" and "+ b +" is "+ c);
+var a = 12;
+var b = 9;
+var c = a/b;
+document.write("division of "+ a +" and "+ b +" is "+ c);
+var a = 12;
+var b = 9;
+var c = a%b;
+document.write("remainder of "+ a +" and "+ b +" is "+ c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
